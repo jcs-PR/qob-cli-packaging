8c3ff64890e33ab7fb335529ea1e64417abcc90c
Jen-Chieh Shen <jcs090218@gmail.com>
2024-11-25 05:44:27 -0800

    Update qob-cli.rb

--- Formula/qob-cli.rb
+++ Formula/qob-cli.rb
@@ -1,19 +1,13 @@
-cask "qob-cli" do
+class QobCli < Formula
+  desc "CLI for building, running, testing, and managing your Emacs Lisp dependencies"
   version "0.1.0"
-  sha256 "9826a4d7f603c0c01692e97950282a2e4e7d12f3d1ba2067ad971cceff4f9aed"
-
-  url "https://github.com/cl-qob/cli/releases/download/#{version}/qob_#{version}_macos-x64.tar.gz"
-  name "Qob CLI"
-  desc "CLI for building, running, testing, and managing your Common Lisp dependencies"
   homepage "https://cl-qob.github.io/"
   license "MIT"
 
-  livecheck do
-    url :url
-    strategy :github_latest
+  url "https://github.com/cl-qob/cli/releases/download/#{version}/qob_#{version}_macos-x64.tar.gz"
+  sha256 "9826a4d7f603c0c01692e97950282a2e4e7d12f3d1ba2067ad971cceff4f9aed"
+
+  def install
+    bin.install_symlink Dir["#{libexec}/*"]
   end
-
-  binary "qob"
-
-  # No zap stanza required
 end
