a71123710f3cbd92b03a12bca60d2b6f7361134e
Jen-Chieh Shen <jcs090218@gmail.com>
2024-11-25 05:24:11 -0800

    Update qob-cli.rb

--- Formula/qob-cli.rb
+++ Formula/qob-cli.rb
@@ -2,7 +2,7 @@ cask "qob-cli" do
   version "0.1.0"
   sha256 "9826a4d7f603c0c01692e97950282a2e4e7d12f3d1ba2067ad971cceff4f9aed"
 
-  url "https://github.com/cl-qob/cli/releases/download/v#{version}/qob_v#{version}_macos-x64.tar.gz"
+  url "https://github.com/cl-qob/cli/releases/download/#{version}/qob_#{version}_macos-x64.tar.gz"
   name "Qob CLI"
   desc "CLI for building, running, testing, and managing your Common Lisp dependencies"
   homepage "https://cl-qob.github.io/"
